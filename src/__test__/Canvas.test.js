6a99e63dee45a4744cfe2c4c682db186fb9f08cf
Gabriel Donas <osradecky@gmail.com>
2021-05-13 15:07:59 +0100

    Start to test React hooks

--- src/__test__/Canvas.test.js
+++ src/__test__/Canvas.test.js
@@ -1,15 +1,30 @@
 import { mount, shallow } from "enzyme";
+import { render, getByTestId } from "@testing-library/react";
 import Canvas from "../components/Canvas";
+import Animation from "../components/Animation";
 
-describe("", () => {
-  it("accepts user account props", () => {
-    const title = "033xz01op";
-    const wrapper = mount(<Canvas animationTitle={title} />);
-    expect(wrapper.props().title).toEqual(title);
+import mockSVG from "../animations/lotties/dots.json";
+
+describe("Test Canvas props", () => {
+  const animationTitle = "Some Title";
+  const animationData = mockSVG;
+
+  it("accepts animation title props", () => {
+    const wrapper = mount(<Canvas animationTitle={animationTitle} />);
+    expect(wrapper.props().animationTitle).toEqual(animationTitle);
   });
-  //it("contains users account email", () => {
-  //  const wrapper = mount(<Account user={user} />);
-  //  const value = wrapper.find("p").text();
-  //  expect(value).toEqual("david@gmail.com");
-  //});
+
+  it("accepts a SVG as animation data props", () => {
+    const wrapper = mount(<Canvas animation={animationData} />);
+    expect(wrapper.props().animation).toEqual(animationData);
+  });
+});
+
+//ISSUE: Find a effective way to test hooks.
+describe("Animation shouldn't start automatically", () => {
+  it("isPaused should be true", () => {
+    const isPaused = true;
+    const wrapper = mount(<Animation isPaused={isPaused} />);
+    expect(wrapper.props().isPaused).toBe(isPaused);
+  });
 });
